1120e68c96a1b3a69b434c694a0312953866f21f
Marco <4high20@gmail.com>
2018-08-15 12:03:32 +0200

    exceeds requirements working

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const basicFieldset = document.querySelector('fieldset');
 const selectDesign = document.querySelector('#design');
 const selectColor = document.querySelector('#color');
 const optionColor = selectColor.children;
+const colorDiv = document.querySelector('#colors-js-puns');
 //declaring activities variables
 const activities = document.querySelector('.activities');
 let sum = 0;
@@ -26,6 +27,7 @@ const cardNumber = document.querySelector('#cc-num');
 const zipCode = document.querySelector('#zip');
 const cvv = document.querySelector('#cvv');
 
+//this function set the default payment to credit card and hides the others
 function defaultPayment(){
   optionPayment[0].style.display = 'none';
   selectPayment.selectedIndex = 1;
@@ -35,16 +37,18 @@ function defaultPayment(){
 
 defaultPayment();
 
+//this function create functionality to count the total amount spent on activities
 function defaultTotal(){
   const total = document.createElement('p');
   total.id = 'total';
   activities.appendChild(total);
-  total.textContent = sum;
+  total.textContent = `Total: $${sum}`;
   total.style.display = 'none';
 }
 
 defaultTotal();
 
+//this function shows as default the "Please select a T-shirt theme" option
 function showDefaultColor() {
   for(let i = 1; i < 7; i++){
     optionColor[i].style.display = 'none';
@@ -54,9 +58,12 @@ function showDefaultColor() {
 
 showDefaultColor();
 
+//hide the other input when the other option is not selected
 inputOther.style.display = 'none';
+//focus on the input name when the app start
 inputName.focus();
 
+//show the other input when "Other" is selected as a Job Role option
 title.addEventListener('change', () => {
   inputOther.style.display = 'none';
   if(title.value === 'other'){
@@ -64,6 +71,10 @@ title.addEventListener('change', () => {
   }
 });
 
+//hide the color selection until a design is selected
+colorDiv.style.display = 'none';
+
+//filters the t-shirt colors based on the choosen design
 selectDesign.addEventListener('change', () => {
   function loopJSPuns(text){
     for(let i = 1; i < 4; i++){
@@ -92,14 +103,19 @@ selectDesign.addEventListener('change', () => {
   }
 
   if(selectDesign.value === 'Select Theme'){
+    colorDiv.style.display = 'none';
     showDefaultColor();
   } else if(selectDesign.value === 'js puns'){
+    colorDiv.style.display = '';
     showPuns();
   } else if(selectDesign.value === 'heart js'){
+    colorDiv.style.display = '';
     showHearts();
   }
 });
 
+//if there are two activities at the same time when one is selected disable the others
+//sum the total spent on activities
 activities.addEventListener('change', (e) => {
   function disableCompeting(name){
     const activity = document.getElementsByName(name)[0]
@@ -141,10 +157,11 @@ activities.addEventListener('change', (e) => {
     total.style.display = 'none';
   } else {
     total.style.display = '';
-    total.textContent = sum;
+    total.textContent = `Total: $${sum}`;
   }
 });
 
+//listen for a change in the select payment and shows only the selected payment div
 selectPayment.addEventListener('change', () => {
   function creditcardPayment(){
     paypalDiv.style.display = 'none';
@@ -176,57 +193,65 @@ cardNumber.setAttribute('type', 'number');
 zipCode.setAttribute('type', 'number');
 cvv.setAttribute('type', 'number');
 
+//this function create an error message for when a wrong input is submitted
+function checkErrorMsg(element, msg, func, parent, next){
+  element.textContent = msg;
+  element.classList.add('error');
+  func(parent, element, next);
+  element.style.display = 'none';
+}
+
+function prepend(parent, child){
+  parent.prepend(child);
+}
+
+function insertBefore(parent, child, nextChild){
+  parent.insertBefore(child, nextChild);
+}
+
+//create all the error messages
 const checkboxError = document.createElement('label');
-checkboxError.textContent = 'You have to select at least 1 activity';
-checkboxError.classList.add('error');
-activities.prepend(checkboxError);
-checkboxError.style.display = 'none';
+checkErrorMsg(checkboxError, 'You have to select at least 1 activity', prepend, activities);
 
 const inputError = document.createElement('label');
-inputError.textContent = 'Insert a Name';
-inputError.classList.add('error');
-basicFieldset.insertBefore(inputError, inputName);
-inputError.style.display = 'none';
+checkErrorMsg(inputError, 'Insert a Name', insertBefore, basicFieldset, inputName);
 
 const emailError = document.createElement('label');
-emailError.textContent = 'Insert a valid email';
-emailError.classList.add('error');
-basicFieldset.insertBefore(emailError, inputEmail);
-emailError.style.display = 'none';
+checkErrorMsg(emailError, 'Insert a valid email', insertBefore, basicFieldset, inputEmail);
 
 const cvvError = document.createElement('label');
-cvvError.textContent = 'Insert a valid cvv number';
-cvvError.classList.add('error');
-paymentFieldset.prepend(cvvError);
-cvvError.style.display = 'none';
+checkErrorMsg(cvvError, 'Insert a valid cvv number', prepend, paymentFieldset);
 
 const zipError = document.createElement('label');
-zipError.textContent = 'Insert a valid zip number';
-zipError.classList.add('error');
-paymentFieldset.prepend(zipError);
-zipError.style.display = 'none';
+checkErrorMsg(zipError, 'Insert a valid zip number', prepend, paymentFieldset);
 
 const cardError = document.createElement('label');
-cardError.textContent = 'Insert a valid card number';
-cardError.classList.add('error');
-paymentFieldset.prepend(cardError);
-cardError.style.display = 'none';
+checkErrorMsg(cardError, 'Insert a credit card number', prepend, paymentFieldset);
 
+const card2Error = document.createElement('label');
+checkErrorMsg(card2Error, 'Insert a valid credit card number between 13 and 16 digits long', prepend, paymentFieldset);
+
+//function to validate email
+function checkEmail(email) {
+  let at = email.indexOf("@");
+  if(at < 1) return false;
+  let dot = email.indexOf(".");
+  if(dot <= at + 2) return false;
+  if (dot === email.length - 1) return false;
+  return true;
+}
+
+//listen for a click on the submit button, prevent the form to be submitted if
+//the required input are not correct
 registerButton.addEventListener('click', (e) => {
   inputError.style.display = 'none';
   checkboxError.style.display = 'none';
   emailError.style.display = 'none';
   cardError.style.display = 'none';
+  card2Error.style.display = 'none';
   zipError.style.display = 'none';
   cvvError.style.display = 'none';
-  function checkEmail(email) {
-    let at = email.indexOf("@");
-    if(at < 1) return false;
-    let dot = email.indexOf(".");
-    if(dot <= at + 2) return false;
-    if (dot === email.length - 1) return false;
-    return true;
-}
+
   let count = 0;
   for(let i = 0; i < checkboxes.length; i++){
     if(checkboxes[i].checked){
@@ -246,12 +271,16 @@ registerButton.addEventListener('click', (e) => {
     e.preventDefault();
   }
   if(selectPayment.value === 'credit card'){
-    if(cardNumber.value.length < 13){
+    if(cardNumber.value.length === 0){
       cardError.style.display = '';
+      console.log('aaaaaaaaaa');
       e.preventDefault();
-    }
-    if(cardNumber.value.length > 16){
-      cardError.style.display = '';
+    } else if (cardNumber.value.length < 13){
+      card2Error.style.display = '';
+      console.log('bbbbbbbbb');
+      e.preventDefault();
+    } else if (cardNumber.value.length > 16){
+      card2Error.style.display = '';
       e.preventDefault();
     }
     if(zipCode.value.length !== 5){
@@ -264,3 +293,14 @@ registerButton.addEventListener('click', (e) => {
     }
   }
 })
+
+//real time email validation checked
+inputEmail.addEventListener('keyup', () => {
+  emailError.style.display = 'none';
+  if(!checkEmail(inputEmail.value)){
+    emailError.style.display = '';
+  }
+  if(inputEmail.value.length === 0){
+    emailError.style.display = 'none';
+  }
+});
